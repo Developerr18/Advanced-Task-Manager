315ae2f9aae66871d2d2c3da629f443c9381a293
Developerr18 <dineshnikum1@gmail.com>
2025-10-07 17:04:48 +0530

    Build UI structure of Header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="bg-indigo-500 max-w-7xl mx-auto">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center p-7">
+      <h1 className="sm:text-4xl text-3xl font-bold mb-2">
+        Advanced Task Manager
+      </h1>
+      <p>Organize your tasks with priority, categories, and smart filtering</p>
+      <div className="flex gap-5 mt-5">
+        <div className="flex flex-col items-center">
+          <div className="text-3xl font-bold">5</div>
+          <div>Total Tasks</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="text-3xl font-bold">1</div>
+          <div>Completed Tasks</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="text-3xl font-bold">4</div>
+          <div>Pending Tasks</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
