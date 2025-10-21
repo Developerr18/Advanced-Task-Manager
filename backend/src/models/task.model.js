2f56668166b967849911ba5670674a7bd38552d5
Developerr18 <dineshnikum1@gmail.com>
2025-10-21 15:29:16 +0530

    feat: create user and task models

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: { type: String, required: true, trim: true },
+  description: { type: String, trim: true },
+  priority: {
+    type: String,
+    enum: ["high", "medium", "low"],
+    default: "medium",
+  },
+  category: String,
+  dueDate: Date,
+  status: {
+    type: String,
+    enum: ["todo", "inProgress", "completed"],
+    default: "todo",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Task", taskSchema);
