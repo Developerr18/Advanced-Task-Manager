5cf92fd7e14beae0c577a8561925e7c580d3c5c7
Developerr18 <dineshnikum1@gmail.com>
2025-10-24 20:34:09 +0530

    feat: implement getTasks function to fetch user tasks

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -1,22 +1,22 @@
 import TaskModel from "../models/taskModel.js";
 
+// create task
 export const createTask = async (req, res) => {
+  const { title, description, priority, category, dueDate, status } = req.body;
+  const userId = req.user.userId;
+
+  if (!title) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Task title is required!" });
+  }
+
   try {
-    const { title, description, category, priority, dueDate, status } =
-      req.body;
-    const userId = req.user.userId;
-
-    if (!title) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Title is required" });
-    }
-
-    const task = await TaskModel.create({
+    const newTask = await TaskModel.create({
       title,
       description: description || "No description",
-      category: category || "other",
       priority: priority || "medium",
+      category: category || "other",
       status: status || "todo",
       dueDate: dueDate ? new Date(dueDate) : null,
       user: userId,
@@ -24,20 +24,46 @@ export const createTask = async (req, res) => {
 
     res.status(201).json({
       success: true,
-      message: "Task created successfully",
-      data: task,
+      message: "Task created successfully!",
+      data: newTask,
     });
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
     res.status(500).json({
       success: false,
-      message: "Server error occured during task creation",
-      error: error.message,
+      message: "Server error occured during task creation!",
+      error: err.message,
     });
   }
 };
 
-export const getTasks = async (req, res) => {};
+// get tasks
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await TaskModel.find({ user: req.user.userId }).sort({
+      createdAt: -1,
+    });
+
+    if (tasks.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "No tasks found!" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Tasks fetched successfully!",
+      data: tasks,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Server error occured during task fetching!",
+      error: err.message,
+    });
+  }
+};
 
 export const updateTask = async (req, res) => {};
 
